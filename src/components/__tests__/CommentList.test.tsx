aabf304727da05b46e52cbe2228426a79407b913
someSOAP <andrey.meg@mail.ru>
2021-04-20 23:09:05 +0300

    CommentList test

--- src/components/__tests__/CommentList.test.tsx
+++ src/components/__tests__/CommentList.test.tsx
@@ -7,18 +7,28 @@ import { RootState } from '../../reducers'
 const WithRootCommentList = withRoot(CommentList)
 
 describe('CommentList', () => {
+    const statesArray: Partial<RootState>[] = [
+        { comments: ['mem', 'kek', 'foo'] },
+        { comments: ['mem', 'kek', 'foo'] },
+    ]
+
     let stateIterator = 0
-    const statesArray: Partial<RootState>[] = [{ comments: ['mem', 'kek', 'foo'] }]
+    let currentState = statesArray[stateIterator]
     let wrapped: ReactWrapper
 
     beforeEach(() => {
-        wrapped = mount(
-            <WithRootCommentList initialState={statesArray[stateIterator]} />
-        )
-        stateIterator++
+        currentState = statesArray[stateIterator]
+        wrapped = mount(<WithRootCommentList initialState={currentState} />)
+        stateIterator = stateIterator + 1
     })
 
     it('creates one <li> pre comment', () => {
         expect(wrapped.find('li').length).toBe(3)
+    })
+
+    it('shows the text for each comment', () => {
+        currentState.comments?.forEach((comment) => {
+            expect(wrapped.render().text()).toContain(comment)
+        })
     })
 })
